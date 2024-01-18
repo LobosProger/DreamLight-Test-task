134d8d342f68291f501cd438d9a8822b4dffd247
LobosProger <78168123+LobosProger@users.noreply.github.com>
2024-01-18 22:57:11 +0600

    Made logic of detecting another plate

--- Assets/Scripts/Elements/Plate element/Picker logic/PlateElementPickerController.cs
+++ Assets/Scripts/Elements/Plate element/Picker logic/PlateElementPickerController.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlateElementPickerController : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class PlateElementPickerController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
 	private PlateElementPickerModel plateElementPickerModel;
 
@@ -23,5 +24,32 @@ public class PlateElementPickerController : MonoBehaviour, IPointerDownHandler, 
 		Vector2 pointerPosition = eventData.position;
 		Vector2 dragPositionForPlate = pointerPosition + plateElementPickerModel.GetOffsetForDraggingElement();
 		transform.position = dragPositionForPlate;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		PlateElementPickerController anotherPlate = null;
+
+		if (IsEnteredInPointerAnotherPlateElement(eventData, out anotherPlate))
+		{
+			Debug.Log($"Entered into: {anotherPlate.name}");
+		}
+	}
+
+	private bool IsEnteredInPointerAnotherPlateElement(PointerEventData eventData, out PlateElementPickerController anotherPlate)
+	{
+		anotherPlate = null;
+		List<RaycastResult> allRaycastedElementsInPointer = new List<RaycastResult>();
+		
+		EventSystem.current.RaycastAll(eventData, allRaycastedElementsInPointer);
+		foreach(RaycastResult eachRaycastedElement in  allRaycastedElementsInPointer)
+		{
+			if(eachRaycastedElement.gameObject != gameObject && eachRaycastedElement.gameObject.TryGetComponent(out anotherPlate))
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
