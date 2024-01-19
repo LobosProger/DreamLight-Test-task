e6741392681d1daeedc4bd52d4ac3701db970ad1
LobosProger <78168123+LobosProger@users.noreply.github.com>
2024-01-19 22:28:26 +0600

    Refactored code

--- Assets/Scripts/Elements/Panel/Saving & loading list/ElementsPanelStorageModel.cs
+++ Assets/Scripts/Elements/Panel/Saving & loading list/ElementsPanelStorageModel.cs
@@ -8,13 +8,18 @@ public class ElementsPanelStorageModel : MonoBehaviour
 {
 	[SerializeField] private string keyOfStoringList;
 
-	public List<PlateElementData> GetSavedListFromMemory()
+	private List<PlateElementDataForJSON> GetSpecialPreparedListOfPlatesFromMemory()
 	{
 		string jsonOfList = PlayerPrefs.GetString(keyOfStoringList);
 		List<PlateElementDataForJSON> retrievingListPlateElementsList = JsonConvert.DeserializeObject<List<PlateElementDataForJSON>>(jsonOfList);
 
+		return retrievingListPlateElementsList;
+	}
+
+	private List<PlateElementData> GetListWithPlatesFromSpecialPreparedListForJSON(List<PlateElementDataForJSON> specialPreparedList)
+	{
 		List<PlateElementData> retrievingList = new List<PlateElementData>();
-		foreach(PlateElementDataForJSON eachPreparedElement in retrievingListPlateElementsList)
+		foreach (PlateElementDataForJSON eachPreparedElement in specialPreparedList)
 		{
 			PlateElementData plateElementData = new PlateElementData(eachPreparedElement.nameOfPlate, eachPreparedElement.numberOfPlate);
 			retrievingList.Add(plateElementData);
@@ -23,18 +28,39 @@ public class ElementsPanelStorageModel : MonoBehaviour
 		return retrievingList;
 	}
 
-	public void SaveCreatedListIntoMemory(List<PlateElementData> retrievingList)
+	private List<PlateElementDataForJSON> PrepareListOfPlatesToListForJSON(List<PlateElementData> plateElementsList)
 	{
 		List<PlateElementDataForJSON> preparingPlateElementsList = new List<PlateElementDataForJSON>();
-		foreach(PlateElementData eachPlateElement in retrievingList)
+		foreach (PlateElementData eachPlateElement in plateElementsList)
 		{
 			PlateElementDataForJSON preparingElementForJSON = new PlateElementDataForJSON(eachPlateElement.GetNameOfPlate(), eachPlateElement.GetNumberOfPlate());
 			preparingPlateElementsList.Add(preparingElementForJSON);
 		}
 
+		return preparingPlateElementsList;
+	}
+
+	private void SavePreparedSpecialListIntoMemory(List<PlateElementDataForJSON> preparingPlateElementsList)
+	{
 		string jsonOfList = JsonConvert.SerializeObject(preparingPlateElementsList);
 		PlayerPrefs.SetString(keyOfStoringList, jsonOfList);
 	}
+
+	public List<PlateElementData> GetSavedListFromMemory()
+	{
+		List<PlateElementDataForJSON> retrievingSpecialPreparedListPlateElementsList = GetSpecialPreparedListOfPlatesFromMemory();
+		List<PlateElementData> retrievingListWithPlates = GetListWithPlatesFromSpecialPreparedListForJSON(retrievingSpecialPreparedListPlateElementsList);
+
+		return retrievingListWithPlates;
+	}
+
+	public void SaveCreatedListIntoMemory(List<PlateElementData> retrievingList)
+	{
+		List<PlateElementDataForJSON> preparingPlateElementsList = PrepareListOfPlatesToListForJSON(retrievingList);
+		SavePreparedSpecialListIntoMemory(preparingPlateElementsList);
+	}
+
+
 
 	// This class was prepared because PlateElementData doesn't allow to convert to JSON during private variables
 	// For it was prepared another class, allows to conducting transformations into JSON format
