20f72e5bdde51a8c0a52359711b37da943398c45
LobosProger <78168123+LobosProger@users.noreply.github.com>
2024-01-19 11:29:51 +0600

    Implementing Observer pattern
    After changing plate elements, there is will be triggered event

---
+++ Assets/Scripts/Elements/Plate element/Events/PlateElementEvents.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateElementEvents : MonoBehaviour
+{
+    public static Action OnChangedPlaceOfPlateElement;
+}
