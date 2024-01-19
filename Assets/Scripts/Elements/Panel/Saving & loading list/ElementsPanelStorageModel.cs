ef0c774f93bebc8c20a3e47f36b8b475db3c518c
LobosProger <78168123+LobosProger@users.noreply.github.com>
2024-01-19 18:30:43 +0600

    Adding newtonsoft package for storage in JSON

--- Assets/Scripts/Elements/Panel/Saving & loading list/ElementsPanelStorageModel.cs
+++ Assets/Scripts/Elements/Panel/Saving & loading list/ElementsPanelStorageModel.cs
@@ -1,8 +1,19 @@
+using PlateElementNamespace;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ElementsPanelStorageModel : MonoBehaviour
 {
-	
+	[SerializeField] private string keyOfStoringList;
+
+	public List<PlateElementData> GetSavedListFromMemory()
+	{
+		return null;
+	}
+
+	public void SaveCreatedListIntoMemory()
+	{
+
+	}
 }
